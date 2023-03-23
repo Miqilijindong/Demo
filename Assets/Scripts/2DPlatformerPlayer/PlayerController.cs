7761a9522be52733faac54dfa7c66da669b4da4e
lee <lee@musegaming.co>
2023-03-23 17:24:53 +0800

    移动改进2，第6部分已完成 加入下墙跳，以及预输入跳跃

--- Assets/Scripts/2DPlatformerPlayer/PlayerController.cs
+++ Assets/Scripts/2DPlatformerPlayer/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float wallSlidingSpeed;
     public float movementForceInAir;
-    public float airDragMultiplier = 0.95f ;
+    public float airDragMultiplier = 0.95f;
     public float variableJumpHeightMultiplier = 0.5f;
 
     /// <summary>
@@ -32,6 +32,9 @@ public class PlayerController : MonoBehaviour
     public float wallCheckDistance;
     public float wallHopForce;
     public float wallJumpForce;
+    public float jumpTimerSet = 0.15f;
+    public float turnTimerSet = 0.1f;
+    public float wallJumpTimerSet = 0.5f;
 
     public LayerMask whatIsGround;
 
@@ -43,15 +46,40 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     [SerializeField]
     private int amountOfJumpsLeft;
-    private int facingDirection = 1 ;
+    /// <summary>
+    /// 当前面对的方向
+    /// 1 = 右   -1 = 左
+    /// </summary>
+    private int facingDirection = 1;
+    /// <summary>
+    /// 判断最后一个墙跳的方向
+    /// </summary>
+    private int lastWallJumpDirection;
+
+    private bool isFaceRight = true;
     private bool isRun;
     private bool isGround;
     private bool isTouchWall;
-    private bool isFaceRight = true;
-
-
-    private bool canJump;
+    private bool canNormalJump;
+    private bool canWallJump;
     private bool isWallSliding;
+    /// <summary>
+    /// 尝试跳跃，当在空中时，按下跳跃键
+    /// 类似于预输入的样子
+    /// </summary>
+    private bool isAttemptingToJump;
+    /// <summary>
+    /// 跳跃后计算按键时间，判断是否大跳
+    /// </summary>
+    private bool checkJumpMultiplier;
+    private bool canMove;
+    private bool canFlip;
+    private bool hasWallJump;
+
+    private float jumpTimer;
+    private float turnTimer;
+    private float wallJumpTimer;
+
 
     // Start is called before the first frame update
     void Start()
@@ -68,36 +96,71 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        InputController();
+        CheckInput();
         CheckMovementDirection();
         CheckIfCanJump();
         CheckIfWallSliding();
+        CheckJump();
     }
 
     private void FixedUpdate()
     {
-        ApplyMovement();
+        ApplyNewMovement();
         UpdateAnimations();
         CheckSurroundings();
     }
 
-    private void InputController()
+    private void CheckInput()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump"))
         {
-            Jump();
+            if (isGround || (amountOfJumpsLeft > 0 && isTouchWall))
+            {
+                NormalJump();
+            }
+            else
+            {// 判断是否预输入，并加入时间判断
+                jumpTimer = jumpTimerSet;
+                isAttemptingToJump = true;
+            }
         }
 
-        if (Input.GetButtonUp("Jump"))
+        if (Input.GetButtonDown("Horizontal") && isTouchWall)
         {
+            if (!isGround && horizontal != facingDirection)
+            {
+                canMove = false;
+                canFlip = false;
+
+                turnTimer = turnTimerSet;
+            }
+        }
+
+        if (!canMove)
+        {
+            turnTimer -= Time.deltaTime;
+
+            if (turnTimer <= 0)
+            {
+                canMove = true;
+                canFlip = true;
+            }
+        }
+
+        //if (Input.GetButtonUp("Jump"))
+        if (checkJumpMultiplier && !Input.GetButton("Jump"))
+        {
+            checkJumpMultiplier = false;
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
         }
     }
 
-    float test;
     Vector3 localScale;
+    /// <summary>
+    /// 旧版的移动方法
+    /// </summary>
     private void ApplyMovement()
     {
         /*if (horizontal != 0)
@@ -113,17 +176,19 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(horizontal * playerMoveSpeed, rb.velocity.y);
         }
+        // 空中移动
         else if (!isGround && !isWallSliding && horizontal != 0)
         {
             Vector2 forceToAdd = new Vector2(horizontal * movementForceInAir, 0);
             rb.AddForce(forceToAdd);
 
-            // 这里以为float不精确，导致有时候会true，直接转向，所以要强转int
+            // 这里空中控制速度，因为float不精确，导致有时候会true，直接转向，所以要强转int
             if (((int)Mathf.Abs(rb.velocity.x)) > playerMoveSpeed)
             {
                 rb.velocity = new Vector3(playerMoveSpeed * horizontal, rb.velocity.y);
             }
         }
+        // 空中有空气阻力
         else if (!isGround && !isWallSliding && horizontal == 0)
         {
             rb.velocity = new Vector2(rb.velocity.x * airDragMultiplier, rb.velocity.y);
@@ -136,28 +201,60 @@ public class PlayerController : MonoBehaviour
                 rb.velocity = new Vector2(rb.velocity.x, -wallSlidingSpeed);
             }
         }
+    }
 
-        if (test != 0 && test == rb.velocity.x * -1)
+    /// <summary>
+    /// 优化后的移动方法
+    /// </summary>
+    private void ApplyNewMovement()
+    {
+        /*if (horizontal != 0)
         {
-            Debug.Log("123");
+            localScale = rb.transform.localScale;
+            // 这里不建议直接使用horizontal，因为后期如果加上了法相天地等变大的技能，转向就会吧Scale变回1
+            localScale.x = MathF.Sign(horizontal);
+            rb.transform.localScale = localScale;
+        }*/
+        if (!isGround && !isWallSliding && horizontal == 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x * airDragMultiplier, rb.velocity.y);
+        }
+        // 只有在地上才可以改变移动
+        else if (canMove)
+        {
+            rb.velocity = new Vector2(horizontal * playerMoveSpeed, rb.velocity.y);
+        }
+
+
+        if (isWallSliding)
+        {
+            if (rb.velocity.y < -wallSlidingSpeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -wallSlidingSpeed);
+            }
         }
     }
 
     private void CheckIfCanJump()
     {
         // 这里要判断是否在地上，同时也要判断Y速率是否为负数，如果是向上的话，就不能补充跳跃次数
-        if ((isGround && rb.velocity.y <= 0) || isWallSliding)
+        if (isGround && rb.velocity.y <= 0.01f)
         {
             amountOfJumpsLeft = amountOfJumps;
         }
 
-        if (amountOfJumpsLeft > 0)
+        if (isTouchWall)
         {
-            canJump = true;
+            canWallJump = true;
+        }
+
+        if (amountOfJumpsLeft <= 0)
+        {
+            canNormalJump = false;
         }
         else
         {
-            canJump = false;
+            canNormalJump = true;
         }
     }
 
@@ -175,7 +272,7 @@ public class PlayerController : MonoBehaviour
 
     private void Flip()
     {
-        if (!isWallSliding)
+        if (!isWallSliding && canFlip)
         {
             facingDirection *= -1;
             isFaceRight = !isFaceRight;
@@ -185,33 +282,106 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        if (canJump && ! isWallSliding/* && rb.velocity.y <= 0*/)
+        if (canNormalJump && !isWallSliding/* && rb.velocity.y <= 0*/)
         {
             //rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             amountOfJumpsLeft--;
         }
-        else if (isWallSliding && horizontal == 0 && canJump)
+        else if (isWallSliding && horizontal == 0 && canNormalJump)
         {
             isWallSliding = false;
             amountOfJumpsLeft--;
             Vector2 forceToAdd = new Vector2(wallHopForce * wallHopDirection.x * -facingDirection, wallHopForce * wallHopDirection.y);
             rb.AddForce(forceToAdd, ForceMode2D.Impulse);
         }
-        else if ((isWallSliding || isTouchWall) && horizontal != 0 && canJump)
+        else if ((isWallSliding || isTouchWall) && horizontal != 0 && canNormalJump)
         {
             isWallSliding = false;
             amountOfJumpsLeft--;
             Vector2 forceToAdd = new Vector2(wallJumpForce * wallJumpDirection.x * horizontal, wallJumpForce * wallJumpDirection.y);
             rb.AddForce(forceToAdd, ForceMode2D.Impulse);
+        }
+    }
 
+    private void CheckJump()
+    {
+        if (jumpTimer > 0)
+        {// 当预输入跳跃时间内判断符合跳跃条件，就跳跃
+            if (!isGround && isTouchWall && horizontal != 0 && horizontal != facingDirection)
+            {
+                WallJump();
+            }
+            else if (isGround)
+            {
+                NormalJump();
+            }
+        }
+        // 当预输入跳跃后，会持续减时间，直到下次跳跃，会很消耗性能，不是很建议这种方法
+        if (isAttemptingToJump)
+        {
+            jumpTimer -= Time.deltaTime;
+        }
+
+        // 墙跳的时间
+        if (wallJumpTimer > 0)
+        {
+            // 当之前墙跳过且向着墙的方向移动的话，向上的力会被取消，意思是单墙不能向上跳
+            if (hasWallJump && horizontal == -lastWallJumpDirection)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0f);
+                hasWallJump = false;
+            }
+            else if (wallJumpTimer <= 0f)
+            {
+                hasWallJump = false;
+            } else
+            {
+                wallJumpTimer -= Time.deltaTime;
+            }
+        }
+    }
+
+    private void NormalJump()
+    {
+        if (canNormalJump)
+        {
+            //rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
+            jumpTimer = 0;
+            isAttemptingToJump = false;
+            checkJumpMultiplier = true;
+        }
+    }
+
+    private void WallJump()
+    {
+        if (canWallJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+            isWallSliding = false;
+            amountOfJumpsLeft = amountOfJumps;
+            amountOfJumpsLeft--;
+            Vector2 forceToAdd = new Vector2(wallJumpForce * wallJumpDirection.x * horizontal, wallJumpForce * wallJumpDirection.y);
+            rb.AddForce(forceToAdd, ForceMode2D.Impulse);
+            jumpTimer = 0;
+            isAttemptingToJump = false;
+            checkJumpMultiplier = true;
+            turnTimer = 0;
+            canMove = true;
+            canFlip = true;
+
+            hasWallJump = true;
+            wallJumpTimer = wallJumpTimerSet;
+            lastWallJumpDirection = -facingDirection;
         }
     }
 
     private void CheckIfWallSliding()
     {
         // 贴着墙，不在地面上，向下掉落时
-        if (isTouchWall && !isGround && rb.velocity.y <= 0)
+        if (isTouchWall && horizontal == facingDirection && rb.velocity.y < 0)
         {
             isWallSliding = true;
         }
