52de204dc0d90e598db1864f66cd9ef1e5d12aca
lee <lee@musegaming.co>
2023-06-06 15:04:53 +0800

    更新2D平台游戏-第36B部分 完成死亡核心组件，目前这个将会是最后一个版本，后续更新的就会是武器系统了，导致大量武器物体会被删除，所以打算另写一个工程吧。

--- Assets/Scripts/2DPlatformerPlayer/Core/Core.cs
+++ Assets/Scripts/2DPlatformerPlayer/Core/Core.cs
@@ -67,11 +67,16 @@ public class Core : MonoBehaviour
     {
         var comp = coreComponents.OfType<T>().FirstOrDefault();
 
-        if (comp == null)
-        {
-            Debug.LogWarning($"{typeof(T)} not found on {transform.parent.name}");
-        }
-        return comp;
+        if (comp)
+            return comp;
+
+        comp = GetComponentInChildren<T>();
+
+        if (comp)
+            return comp;
+
+        Debug.LogWarning($"{typeof(T)} not found on {transform.parent.name}");
+        return null;
     }
 
     /// <summary>
